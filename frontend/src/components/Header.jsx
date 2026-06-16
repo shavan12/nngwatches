20c74a97d0947744c7a23d81dc1be22ffec3306d
shavan12 <shavan1277@gmail.com>
2026-06-16 16:36:49 +0300

    update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -99,7 +99,7 @@ export default function Header() {
 
             {/* Logo */}
             <button onClick={() => navigate('/')} style={{ display: 'flex', flexDirection: 'column', alignItems: dir === 'rtl' ? 'flex-end' : 'flex-start', gap: 2, flexShrink: 0 }}>
-              <img src="https://www.imghippo.com/i/UxT3851jmw.png" alt="NG" style={{height: 40,width: 'auto',objectFit: 'contain',cursor: 'pointer',}}onClick={() => navigate('/')}
+              <img src="/NNGF.webp" alt="NNG" style={{height: 40,width: 'auto',objectFit: 'contain',cursor: 'pointer',}}onClick={() => navigate('/')}
 />
             </button>
 
